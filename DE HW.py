9868982199e5ae1fd9b0d319a5880be6bd1cc96f
Spiderwebs <ahm20178110@std.psut.edu.jo>
2018-04-07 02:42:56 +0300

    DE HW
    First DE HW on GitHub

---
+++ DE HW.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  6 04:03:48 2018
+
+@author: Spiderwebs
+"""
+
+import cx_Oracle
+
+with open('GEODATASOURCE-COUNTRY.TXT') as f:
+    content = f.readlines()
+    
+contList = []
+
+for rows in content:
+    row = rows.split(sep='\t')
+    contList.append(row)
+
+for x in range(0,len(contList)):
+    contList[x][-1] = contList[x][-1][0:-1]
+
+
+con = cx_Oracle.connect('Spiderwebs/9137@localhost:1521')
+
+cur=con.cursor()
+
+cur.executemany("INSERT INTO COUNTRIES VALUES(:1,:2,:3,:4)",contList)
+
+con.commit ()
+
+cur.close()
